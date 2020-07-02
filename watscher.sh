466eea3bf282fe023d774d7121b632a6b611b1c4
jti989 <58518356+jti989@users.noreply.github.com>
2020-07-02 14:38:27 +0200

    Create watscher.sh

---
+++ watscher.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+today=$(date +%Y-%m-%d)
+yesterday=$(date -d "yesterday" +'%Y-%m-%d')
+
+# Read the input file and download all the changelogs
+while read -r line
+
+do
+  echo $line
+  name=${line%,*}
+  url=${line##*,}
+  curl -o "logs/$name-$today.txt" "$url"
+  if ! cmp "logs/$name-$yesterday.txt" "logs/$name-$today.txt" >/dev/null 2>&1
+  then
+    echo $name > logs/$name.diff.txt
+    echo ""  >> logs/$name.diff.txt
+    diff "logs/$name-$yesterday.txt" "logs/$name-$today.txt" | sed 's/> //' >> logs/$name.diff.txt
+  fi
+done < watchlist.csv
+
+if ls logs/*.diff.txt 1> /dev/null 2>&1
+then
+
+  echo "To: test@example.com" > mail.txt
+  echo "Subject: Daily changelog update" >> mail.txt
+  echo "From: changelog@example.com" >> mail.txt
+  echo "" >> mail.txt
+  cat logs/*.diff.txt >> mail.txt
+
+  sendmail -vt < mail.txt
+
+  rm mail.txt
+  rm logs/*.diff.txt
+
+fi
+
+rm logs/*-$yesterday.txt
